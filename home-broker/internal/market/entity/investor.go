e9a6c21cdf0de0bdf657250c53c73d15120f03d5
Gabriel Dantas <gabrieldantas@Dantas-Macbook.local>
2023-06-21 18:13:44 -0300

    making investor.go

--- home-broker/internal/market/entity/investor.go
+++ home-broker/internal/market/entity/investor.go
@@ -11,9 +11,38 @@ type InvestorAssetPosition struct {
 	Shares  int
 }
 
-func newInvestor(id string) *Investor {
+func NewInvestor(id string) *Investor {
 	return &Investor{
 		ID:            id,
 		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) GetAssetPosition(assetId string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetID == assetId {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
+func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
+}
+
+func (i *Investor) UpdateAssetPosition(assetID string, qtdShares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, qtdShares))
+	} else {
+		assetPosition.Shares += qtdShares
 	}
 }
