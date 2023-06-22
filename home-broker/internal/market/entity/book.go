db2ff7049695f0da1457c6138837bec4c54d05c8
Gabriel Dantas <gabrieldantas@Dantas-Macbook.local>
2023-06-22 16:50:39 -0300

    inicia função de trade

--- home-broker/internal/market/entity/book.go
+++ home-broker/internal/market/entity/book.go
@@ -1,17 +1,54 @@
 package entity
 
-import "sync"
+import (
+	"container/heap"
+	"hash"
+	"sync"
+)
 
 type Book struct {
-	Order        []*Order
-	Transaction  []*Transaction
-	OrderChan    chan *Order
-	OrderChanOut chan *Order
-	Wg           *sync.WaitGroup
+	Order         []*Order
+	Transactions  []*Transaction
+	OrdersChan    chan *Order
+	OrdersChanOut chan *Order
+	Wg            *sync.WaitGroup
 }
 
-func NewGroup(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book {
-	return &Book {
-		
+func NewBook(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Order:         []*Order{},
+		Transactions:  []*Transaction{},
+		OrdersChan:    orderChan,
+		OrdersChanOut: orderChanOut,
+		Wg:            wg,
 	}
+}
+
+func (b *Book) Trade() {
+	buyOrders := NewOrderQueue()
+	sellOrders := NewOrderQueue()
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+	for order := range b.OrdersChan {
+		if order.OrderType == "BUY" {
+			buyOrders.Push(order)
+
+			if sellOrders.Len() > 0 && sellOrders[0].Price <= order.Price
+			sellOrder := sellOrders.Pop().(*Order)
+
+			if sellOrder.PendingShares > 0 {
+				transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+				b.AddTransaction(transaction, b.Wg)
+				sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+				order.Transactions = append(order.Transactions, transaction)
+				b.OrdersChanOut <- sellOrder
+				b.OrdersChanOut <- order
+
+				if sellOrder.PendingShares > 0 {
+					sellOrders.Push(sellOrder)
+				}
+			}
+		} 
 }
